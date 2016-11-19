3f08effe39d71193aa198c7ad93b8de0cc475a52
Blagoja Evkoski <Blagoja.Evkoski@netcetera.com>
2016-11-19 14:00:25 +0100

    Implement initial tests for the update functionalities

---
+++ test/update.spec.js
@@ -0,0 +1,87 @@
+'use strict';
+
+import { expect } from './support/utils';
+import { fromJS } from 'immutable';
+import Cursor from 'immutable/contrib/cursor';
+import update from '../src/update';
+
+describe('Updates', function() {
+
+  it('registers an update', function() {
+    const action1 = {
+      fromKind: ['article', 'specific'],
+      type: 'TOGGLE_BOOKMARK'
+    };
+    const action2 = {
+      fromKind: ['article'],
+      type: '.LOAD'
+    };
+
+    update.register(action1.fromKind, elementRegistry => {
+      elementRegistry.register(action1.type, (element, action) => console.log('Toggled!'));
+    });
+    update.register(action2.fromKind, elementRegistry => {
+      elementRegistry.register(action2.type, (element, action) => console.log('Loading...'));
+    });
+    update.register(action2.fromKind, elementRegistry => {
+      elementRegistry.register(action2.type, (element, action) => console.log('Loading...'));
+    });
+    expect(update.forAction(action1)).to.exist;
+    expect(update.forAction(action2)).to.exist;
+    expect(update.forAction({ fromKind: 'unknown' })).not.to.exist;
+
+    update.register(action1.fromKind, elementRegistry => {
+      elementRegistry.register(action1.type, null);
+    });
+    update.register(action2.fromKind, elementRegistry => {
+      elementRegistry.register(action2.type, null);
+    });
+    expect(update.forAction(action1)).not.to.exist;
+    expect(update.forAction(action2)).not.to.exist;
+  });
+
+  it('returns a cursor upon invocation of the reducer function', function() {
+    const data = fromJS({
+      element1: {
+        element2: {
+          element3: {
+            kind: ['article', 'specific']
+          },
+          kind: ['container']
+        },
+        kind: ['container']
+      }
+    });
+    const cursor = Cursor.from(data, ['element1'], () => {});
+    const localUpdate = {
+      fromKind: ['article', 'specific'],
+      fromPath: ['element2', 'element3'],
+      type: 'TOGGLE_BOOKMARK'
+    };
+    const globalUpdate = {
+      fromKind: ['container'],
+      fromPath: [],
+      type: '.TOGGLE_BOOKMARK'
+    };
+    const readUpdate = {
+      fromKind: ['container'],
+      fromPath: [],
+      type: 'READ'
+    };
+    update.register(localUpdate.fromKind, elementRegistry => {
+      elementRegistry.register(localUpdate.type, (element, action) => {});
+    });
+    update.register(globalUpdate.fromKind, elementRegistry => {
+      elementRegistry.register(globalUpdate.type, (element, action) => {});
+    });
+    update.register(readUpdate.fromKind, elementRegistry => {
+      elementRegistry.register(readUpdate.type, (element, action) => {});
+    });
+
+    expect(update.reducer(cursor, {})).to.equal(cursor);
+    expect(update.reducer(cursor, localUpdate)).to.exist;
+    expect(update.reducer(cursor, globalUpdate)).to.exist;
+    expect(update.reducer(cursor, readUpdate)).to.exist;
+  });
+
+});
