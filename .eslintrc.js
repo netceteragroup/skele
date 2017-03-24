304609f921aad170f9bdf7230b3056b92091630e
Ognen Ivanovski <ognen.ivanovski@netcetera.com>
2017-03-24 15:02:54 +0100

    eslint upgrade

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  parser: 'babel-eslint',
+
+  env: {
+    node: true,
+    es6: true,
+    browser: true,
+    jest: true
+  },
+
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended"
+  ],
+
+  plugins: [
+    'promise',
+    'react',
+    'react-native'
+  ]
+}
