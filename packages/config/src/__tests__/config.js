2c5ba6526035f12815a7c1da740be26182f63743
Bojana Popovska <bojana.popovska@netcetera.com>
2017-05-25 11:55:24 +0200

    Implement configuration mechanism - handle multiple configurations in main config object

--- packages/config/src/__tests__/config.js
+++ packages/config/src/__tests__/config.js
@@ -4,7 +4,11 @@ import { define, init, activeConfiguration } from '../config';
 
 describe('config', () => {
   test('single layer, default profile', () => {
-    const testConfiguration = {a: 1}
+    const testConfiguration = {
+      config1: {
+        a: 1
+      }
+    }
     define(testConfiguration)
     init()
     expect(activeConfiguration).toEqual(testConfiguration);
@@ -12,28 +16,36 @@ describe('config', () => {
 
   test('multiple layers, single profile', () => {
     const layerOneConfig = {
-      a: 1,
-      b: 2,
-      profiles: {
-        dev: {
-          a: 5,
-          b: 6
+      config1: {
+        a: 1,
+        b: 2,
+        profiles: {
+          dev: {
+            a: 5,
+            b: 6
+          }
         }
       }
     }
     const layerTwoConfig = {
-      b: 3
+      config1: {
+        b: 3
+      }
     }
     const layerThreeConfig = {
-      profiles: {
-        dev: {
-          b: 10
+      config1: {
+        profiles: {
+          dev: {
+            b: 10
+          }
         }
       }
     }
     const expectedConfig = {
-      a: 5,
-      b: 10
+      config1: {
+        a: 5,
+        b: 10
+      }
     }
     define(layerOneConfig)
     define(layerTwoConfig)
@@ -44,30 +56,34 @@ describe('config', () => {
 
   test('one layer, multiple profiles', () => {
     const config = {
-      a: 1,
-      b: 2,
-      c: 15,
-      profiles: {
-        dev: {
-          a: 5,
-          b: 6
-        },
-        prod: {
-          b: 10
-        },
-        android: {
-          a: 6,
-          b: 7
-        },
-        ios: {
-          b: 11
+      config1: {
+        a: 1,
+        b: 2,
+        c: 15,
+        profiles: {
+          dev: {
+            a: 5,
+            b: 6
+          },
+          prod: {
+            b: 10
+          },
+          android: {
+            a: 6,
+            b: 7
+          },
+          ios: {
+            b: 11
+          }
         }
       }
     }
     const expectedConfig = {
-      a: 6,
-      b: 10,
-      c: 15
+      config1: {
+        a: 6,
+        b: 10,
+        c: 15
+      }
     }
     define(config)
     init(['prod', 'android'])
@@ -76,48 +92,131 @@ describe('config', () => {
 
   test('multiple layers, multiple profiles', () => {
     const layerOneConfig = {
-      a: 1,
-      b: 2,
-      c: 3,
-      profiles: {
-        prod: {
-          b: 6
-        },
-        ios: {
-          b: 7
-        },
-        android: {
-          a: 8
+      config1: {
+        a: 1,
+        b: 2,
+        c: 3,
+        profiles: {
+          prod: {
+            b: 6
+          },
+          ios: {
+            b: 7
+          },
+          android: {
+            a: 8
+          }
         }
       }
     }
 
     const layerTwoConfig = {
-      a: 10,
-      b: 11,
-      profiles: {
-        dev: {
-          b: 14
-        },
-        prod: {
-          a: 15
-        },
-        ios: {
-          a: 16
-        },
-        android: {
-          b: 17
+      config1: {
+        a: 10,
+        b: 11,
+        profiles: {
+          dev: {
+            b: 14
+          },
+          prod: {
+            a: 15
+          },
+          ios: {
+            a: 16
+          },
+          android: {
+            b: 17
+          }
         }
       }
     }
+
     const expectedConfig = {
-      a: 16,
-      b: 14,
-      c: 3
+      config1: {
+        a: 16,
+        b: 14,
+        c: 3
+      }
     }
     define(layerOneConfig)
     define(layerTwoConfig)
     init(['dev', 'ios'])
     expect(activeConfiguration).toEqual(expectedConfig);
   });
+
+  test('multiple configurations', () => {
+    const layerOneConfig = {
+      config1: {
+        a: 1,
+        b: 2,
+        c: 3,
+        profiles: {
+          prod: {
+            b: 6
+          },
+          ios: {
+            b: 7
+          },
+          android: {
+            a: 8
+          }
+        }
+      }
+    }
+
+    const layerTwoConfig = {
+      config1: {
+        a: 10,
+        b: 11,
+        profiles: {
+          dev: {
+            b: 14
+          },
+          prod: {
+            a: 15
+          },
+          ios: {
+            a: 16
+          },
+          android: {
+            b: 17
+          }
+        }
+      },
+      config2: {
+        c: 5,
+        profiles: {
+          dev: {
+            c: 10
+          },
+          prod: {
+            c: 1
+          },
+          ios: {
+            c: 2
+          },
+          android: {
+            c: 3
+          }
+        }
+      }
+    }
+
+    const expectedConfig = {
+      config1: {
+        a: 16,
+        b: 14,
+        c: 3
+      },
+      config2: {
+        c: 10
+      }
+    }
+
+    define(layerOneConfig)
+    define(layerTwoConfig)
+    init(['dev', 'ios'])
+    expect(activeConfiguration).toEqual(expectedConfig);
+  });
+
 });
--- packages/config/src/config.js
+++ packages/config/src/config.js
@@ -11,18 +11,25 @@ export function define(configuration) {
 
 export function init(profiles) {
 
+  // reverse the order of the profiles, least important handled first
+  // and overridden with values from the more important profiles
   if (profiles) profiles.reverse()
 
    layers.forEach(layer => {
-     // values from (default) profile
-     const defaultProfileForLayer = pickBy((val, key) => key !== 'profiles', layer)
-     activeConfiguration = deepMerge(activeConfiguration, defaultProfileForLayer)
-     if(profiles) {
-       profiles.forEach(profile => {
-         if (layer['profiles']) {
-           activeConfiguration = deepMerge(activeConfiguration, layer['profiles'][profile])
+     Object.entries(layer).forEach(
+       ([key, configOfLayer]) => {
+         // values from (default) profile
+         const defaultProfileForLayer = pickBy((val, key) => key !== 'profiles', configOfLayer)
+         activeConfiguration[key] = deepMerge(activeConfiguration[key], defaultProfileForLayer)
+         // if there are specific profiles available fetch the values from them
+         if (profiles) {
+           profiles.forEach(profile => {
+             if (configOfLayer['profiles']) {
+               activeConfiguration[key] = deepMerge(activeConfiguration[key], configOfLayer['profiles'][profile])
+             }
+           })
          }
-       })
-     }
+       }
+     )
    })
 }
