b5478026c58ee8cb3d69b8b86bed32ad8c75f7ec
Bojana Popovska <bojana.popovska@netcetera.com>
2017-05-29 08:43:10 +0200

    Remove mutable change of function argument

--- packages/config/src/config.js
+++ packages/config/src/config.js
@@ -1,4 +1,4 @@
-import { pickBy } from 'ramda'
+import { pickBy, reverse } from 'ramda'
 import deepMerge from './utils/merge'
 
 let layers = []
@@ -10,28 +10,31 @@ function define(configuration) {
 }
 
 function init(profiles) {
+  let activeProfiles = profiles
 
   // reverse the order of the profiles, least important handled first
   // and overridden with values from the more important profiles
-  if (profiles) profiles.reverse()
+  if (profiles){
+    activeProfiles = reverse(profiles)
+  }
 
-   layers.forEach(layer => {
-     Object.entries(layer).forEach(
-       ([key, configOfLayer]) => {
-         // values from (default) profile
-         const defaultProfileForLayer = pickBy((val, key) => key !== 'profiles', configOfLayer)
-         activeConfiguration[key] = deepMerge(activeConfiguration[key], defaultProfileForLayer)
-         // if there are specific profiles available fetch the values from them
-         if (profiles) {
-           profiles.forEach(profile => {
-             if (configOfLayer['profiles']) {
-               activeConfiguration[key] = deepMerge(activeConfiguration[key], configOfLayer['profiles'][profile])
-             }
-           })
-         }
-       }
-     )
-   })
+  layers.forEach(layer => {
+    Object.entries(layer).forEach(
+      ([key, configOfLayer]) => {
+        // values from (default) profile
+        const defaultProfileForLayer = pickBy((val, key) => key !== 'profiles', configOfLayer)
+        activeConfiguration[key] = deepMerge(activeConfiguration[key], defaultProfileForLayer)
+        // if there are specific profiles available fetch the values from them
+        if (activeProfiles) {
+          activeProfiles.forEach(profile => {
+            if (configOfLayer['profiles']) {
+              activeConfiguration[key] = deepMerge(activeConfiguration[key], configOfLayer['profiles'][profile])
+            }
+          })
+        }
+      }
+    )
+  })
 }
 
 export {
