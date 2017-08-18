6085427b0ae5a810fbbd9a2229bc1649192c89e9
Ognen Ivanovski <ognen.ivanovski@netcetera.com>
2017-08-18 16:11:25 +0200

    make eslint happy again

--- packages/core/src/__tests__/elementZipper.js
+++ packages/core/src/__tests__/elementZipper.js
@@ -1,13 +1,11 @@
-'use strict';
+'use strict'
 
 import { fromJS } from 'immutable'
 import { zip, data } from '..'
 
-const { canGoDown } = zip
 const childCollectionKind = '@@girders-elements/child-collection'
 
 describe('Zipper', () => {
-
   const singleChild = {
     kind: 'parent',
     children: [
@@ -15,11 +13,11 @@ describe('Zipper', () => {
         kind: 'lvl1',
         children: [
           {
-            kind: 'lvl2'
-          }
-        ]
-      }
-    ]
+            kind: 'lvl2',
+          },
+        ],
+      },
+    ],
   }
 
   it('zipper should correctly navigate up and down', () => {
@@ -27,60 +25,64 @@ describe('Zipper', () => {
     expect(zipper.value().get('kind')).toEqual('parent')
     expect(data.isOfKind(childCollectionKind, zipper.down().value())).toBe(true)
     expect(zipper.down().down().value().get('kind')).toEqual('lvl1')
-    expect(data.isOfKind(childCollectionKind, zipper.down().down().down().value())).toBe(true)
-    expect(zipper.down().down().down().down().value().get('kind')).toEqual('lvl2')
+    expect(
+      data.isOfKind(childCollectionKind, zipper.down().down().down().value())
+    ).toBe(true)
+    expect(zipper.down().down().down().down().value().get('kind')).toEqual(
+      'lvl2'
+    )
     expect(zipper.down().down().down().down().down()).toBeNull()
     expect(zipper.down().up().value().get('kind')).toEqual('parent')
     expect(zipper.down().down().down().up().value().get('kind')).toEqual('lvl1')
-  });
+  })
 
   const multipleChildren = {
     id: 1,
-    kind: "t",
+    kind: 't',
     children: [
       {
         id: 2,
-        kind: "t",
+        kind: 't',
         children: [
           {
             id: 3,
-            kind: "t"
+            kind: 't',
           },
           {
-            kind: "t",
-            id: 4
-          }
-        ]
+            kind: 't',
+            id: 4,
+          },
+        ],
       },
       {
         id: 5,
-        kind: "t",
+        kind: 't',
         children: [
           {
-            kind: "t",
-            id: 6
+            kind: 't',
+            id: 6,
           },
           {
-            kind: "t",
-            id: 7
-          }
-        ]
+            kind: 't',
+            id: 7,
+          },
+        ],
       },
       {
         id: 8,
-        kind: "t",
+        kind: 't',
         children: [
           {
-            kind: "t",
-            id: 9
+            kind: 't',
+            id: 9,
           },
           {
-            kind: "t",
-            id: 10
-          }
-        ]
-      }
-    ]
+            kind: 't',
+            id: 10,
+          },
+        ],
+      },
+    ],
   }
 
   it('zipper should correctly navigate up down left and right', () => {
@@ -91,37 +93,48 @@ describe('Zipper', () => {
     expect(zipper.down().down().value().get('id')).toEqual(2)
     expect(zipper.down().down().right().value().get('id')).toEqual(5)
     expect(zipper.down().down().right().right().value().get('id')).toEqual(8)
-    expect(zipper.down().down().right().right().left().value().get('id')).toEqual(5)
-    expect(data.isOfKind(childCollectionKind, zipper.down().down().right().right().left().up().value())).toBe(true)
-    expect(zipper.down().down().right().right().left().up().up().value().get('id')).toEqual(1)
-  });
+    expect(
+      zipper.down().down().right().right().left().value().get('id')
+    ).toEqual(5)
+    expect(
+      data.isOfKind(
+        childCollectionKind,
+        zipper.down().down().right().right().left().up().value()
+      )
+    ).toBe(true)
+    expect(
+      zipper.down().down().right().right().left().up().up().value().get('id')
+    ).toEqual(1)
+  })
 
   const multipleChildrenElements = {
     id: 1,
-    kind: "t",
+    kind: 't',
     left: [
       {
-        kind: "t",
-        id: 2
-      }
+        kind: 't',
+        id: 2,
+      },
     ],
     right: [
       {
-        kind: "t",
-        id: 3
+        kind: 't',
+        id: 3,
       },
       {
-        kind: "t",
-        id: 4
-      }
-    ]
+        kind: 't',
+        id: 4,
+      },
+    ],
   }
 
   it('zipper multiple children elements', () => {
-    const zipper = zip.elementZipper(fromJS(multipleChildrenElements), ['left', 'right'])
+    const zipper = zip.elementZipper(fromJS(multipleChildrenElements), [
+      'left',
+      'right',
+    ])
 
     expect(zipper.value().get('id')).toEqual(1)
-    console.log(zipper.canGoDown(zipper))
     expect(zipper.down().value().get('propertyName')).toEqual('left')
 
     expect(zipper.down().right().value().get('propertyName')).toEqual('right')
@@ -129,37 +142,35 @@ describe('Zipper', () => {
     expect(zipper.down().down().value().get('id')).toEqual(2)
     expect(zipper.down().right().down().value().get('id')).toEqual(3)
     expect(zipper.down().right().down().right().value().get('id')).toEqual(4)
-  });
+  })
 
   const withChildrenPositions = {
     id: 1,
-    kind: "tX",
-    "@@girders-elements/children": ['left', 'right'],
+    kind: 'tX',
+    '@@girders-elements/children': ['left', 'right'],
 
     left: [
       {
-        kind: "t",
-        id: 2
-      }
+        kind: 't',
+        id: 2,
+      },
     ],
     right: [
       {
-        kind: "t",
-        id: 3
+        kind: 't',
+        id: 3,
       },
       {
-        kind: "t",
-        id: 4
-      }
-    ]
+        kind: 't',
+        id: 4,
+      },
+    ],
   }
-
 
   it('supports the @@girders-elements/children hint for child positions', () => {
     const zipper = zip.elementZipper(fromJS(withChildrenPositions))
 
     expect(zipper.value().get('id')).toEqual(1)
-    console.log(zipper.canGoDown(zipper))
     expect(zipper.down().value().get('propertyName')).toEqual('left')
 
     expect(zipper.down().right().value().get('propertyName')).toEqual('right')
@@ -167,5 +178,5 @@ describe('Zipper', () => {
     expect(zipper.down().down().value().get('id')).toEqual(2)
     expect(zipper.down().right().down().value().get('id')).toEqual(3)
     expect(zipper.down().right().down().right().value().get('id')).toEqual(4)
-  });
-});
+  })
+})
