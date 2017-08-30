d300d2cee4ac2f1772f8c8f5f58e3dd1ceeb1035
Ognen Ivanovski <ognen.ivanovski@netcetera.com>
2017-08-30 22:30:06 +0200

    refactored withListeners -> WithEvents

--- packages/components/src/index.js
+++ packages/components/src/index.js
@@ -1,22 +1,26 @@
-'use strict';
+'use strict'
 
-import ViewportTracker from './viewport/tracker';
-import ViewportAware from './viewport/aware';
-import WithPlaceholder from './viewport/withPlaceholder';
-import WithListeners from './shared/withListeners';
+import ViewportTracker from './viewport/tracker'
+import ViewportAware from './viewport/aware'
+import WithPlaceholder from './viewport/withPlaceholder'
+import WithEvents from './shared/withEvents'
+i
 
-const ViewportTrackerWithListeners = WithListeners(ViewportTracker);
+const ViewportTrackerWithListeners = WithListeners(ViewportTracker)
 
 const Viewport = {
   Tracker: ViewportTrackerWithListeners,
   Aware: ViewportAware,
   WithPlaceholder: WithPlaceholder,
-};
+}
 
-export {
-  Viewport,
-};
+const Mixins = {
+  WithEvents,
+}
+
+export { Viewport, Mixins }
 
 export default {
   Viewport,
-};
+  Mixins,
+}
---
+++ packages/components/src/shared/WithEvents.js
@@ -0,0 +1,128 @@
+'use strict'
+
+import PropTypes from 'prop-types'
+
+const listeners = '@@girders-elements.internal/listeners'
+
+/**
+ * A mxin that adds event handling methods to the a react component
+ *
+ * @param eventDefinitons an array of event definitions or a single event definitions
+ * @param OriginalComponent
+ *
+ * an event defintion is either
+ * - a string, representing the event name
+ * - an object with the following properties:
+ *   - name: the event name (required)
+ *   - isChildContext: whether the event should be exposed in childContext
+ *     defaults to false
+ *   - addMethod: name of the add listener method (optional)
+ *   - removeMethod: name of the remove istener method (optional)
+ *   - notifyMethod: name of the notify listeners method (optional)
+ */
+export default (eventDefinitons, OriginalComponent) => {
+  const defs = normalizeEventDefinitions(eventDefinitons)
+  const inChildContext = defs.filter(d => d.inChildContext)
+
+  class Derived extends OriginalComponent {
+    constructor(props, context) {
+      super(props, context)
+
+      this[listeners] = {}
+
+      defs.forEach(d => {
+        this[listeners][d.name] = []
+        this[d.addMethod] = this[d.addMethod].bind(this)
+        this[d.removeMethod] = this[d.removeMethod].bind(this)
+        this[d.notifyMethod] = this[d.notifyMethod].bind(this)
+      })
+    }
+
+    getChildContext() {
+      const orig = super.getChildContext && super.getChildContext()
+
+      if (inChildContext.length > 0) {
+        let derived = { ...orig }
+
+        inChildContext.forEach(d => {
+          derived[d.addMethod] = this[d.addMethod]
+          derived[d.removeMethod] = this[d.removeMethod]
+        })
+
+        return derived
+      }
+
+      return orig
+    }
+
+    static displayName = `WithListeners(${OriginalComponent.displayName ||
+      OriginalComponent.name ||
+      'Component'})`
+
+    static propTypes = OriginalComponent.propTypes
+  }
+
+  // Add event handling methods
+
+  defs.forEach(function(d) {
+    Derived.prototype[d.addMethod] = function(callback) {
+      if (this[listeners][d.name].indexOf(callback === -1)) {
+        this[listeners][d.name].push(callback)
+      }
+    }
+
+    Derived.prototype[d.removeMethod] = function(callback) {
+      const index = this[listeners][d.name].indexOf(callback)
+      if (index !== -1) {
+        this[listeners][d.name].splice(index, 1)
+      }
+    }
+
+    Derived.prototype[d.notifyMethod] = function(evt) {
+      this[listeners][d.name].forEach(callback => callback(evt))
+    }
+  })
+
+  // add Child contextTypes
+
+  Derived.childContextTypes = {
+    ...OriginalComponent.childContextTypes,
+  }
+
+  if (inChildContext.length > 0) {
+    inChildContext.forEach(d => {
+      Derived.childContextTypes[d.addMethod] = PropTypes.func
+      Derived.childContextTypes[d.removeMethod] = PropTypes.func
+    })
+  }
+
+  return Derived
+}
+
+function normalizeEventDefinitions(eventDefs) {
+  let defs = eventDefs
+
+  if (!Array.isArray(defs)) {
+    defs = [defs]
+  } else if (defs == null) {
+    defs = []
+  }
+
+  defs = defs.map(d => (typeof d === 'string' ? { name: d } : d))
+  defs.forEach(d => (d.name = capitalize(d.name)))
+
+  defs = defs.map(d => ({
+    inChildContext: false,
+    addMethod: `add${d.name}Listener`,
+    removeMethod: `remove${d.name}Listener`,
+    notifyMethod: `notify${d.name}Listeners`,
+
+    ...d,
+  }))
+
+  return defs
+}
+
+function capitalize(s) {
+  return s && s[0].toUpperCase() + s.slice(1)
+}
