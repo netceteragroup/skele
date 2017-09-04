976edc2bae69f7420e636ab37b4c6d149b45fa31
Ognen Ivanovski <ognen.ivanovski@netcetera.com>
2017-09-04 15:20:06 +0200

    kernel, subsystem WIP

---
+++ packages/core/src/kernel.js
@@ -0,0 +1,20 @@
+'use strict'
+
+import invariant from './impl'
+// what's a kernel
+// -- store
+// -- dispatch at
+// -- registered subsystenms
+
+export function create(init, subsystems, ) {
+
+}
+
+export function extend() {
+
+}
+
+export default  {
+  create,
+  extend
+}
---
+++ packages/core/src/subsystem.js
@@ -0,0 +1,19 @@
+'use strict'
+
+// what is a subsystem
+// -- middleware (optional)
+// -- reducer (optional)
+// -- updates
+// -- effects
+// -- ui
+// -- element mixins
+// -- engine mixins
+// -- name
+// -- custom methods
+
+class SubSystem {
+  constructor(name) {
+    super()
+    this.name = name
+  }
+}
