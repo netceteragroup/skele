678d630767aac35e7fe63b65a2bfe7fa4b2fe506
Blagoja Evkoski <blagoja.evkoski@netcetera.com>
2017-09-17 20:54:06 +0200

    fix(components): Improve viewport awareness higher-order component (#62)
    * Optimise layout measurements of viewport aware components
    
    This commit will introduce an optimisation that will greatly improve
    the performance of the onViewportChange calculations.
    
    * Fix setting of state on unmounted viewport aware components
    
    This commit will make sure that the state of unmounted components is
    never set. This would sometimes happen when the UIManager.measureLayout
    callback gets invoked and the component has already unmounted.
    
    * Improve readability of onViewportChange
    
    This commit will improve the readability of the code in the function.

--- packages/components/src/viewport/aware/index.js
+++ packages/components/src/viewport/aware/index.js
@@ -10,6 +10,8 @@ export default WrappedComponent => {
     constructor(props, context) {
       super(props, context)
       this.state = {
+        componentOffset: null,
+        componentHeight: null,
         inViewport: false,
       }
     }
@@ -17,30 +19,50 @@ export default WrappedComponent => {
     componentDidMount() {
       this.context.addViewportListener &&
         this.context.addViewportListener(this._onViewportChange)
+      this._isMounted = true
     }
 
     componentWillUnmount() {
       this.context.removeViewportListener &&
         this.context.removeViewportListener(this._onViewportChange)
+      this._isMounted = false
     }
 
     _onViewportChange = info => {
-      if (this.wrapperRef != null) {
+      if (!this.nodeHandle) {
+        return
+      }
+      if (
+        this.state.componentOffset !== null &&
+        this.state.componentHeight !== null
+      ) {
+        this.setState({
+          inViewport: Utils.isInViewport(
+            info.viewportOffset,
+            info.viewportHeight,
+            this.state.componentOffset,
+            this.state.componentHeight,
+            this.props.preTriggerRatio
+          ),
+        })
+      } else {
         UIManager.measureLayout(
           this.nodeHandle,
           info.parentHandle,
           () => {},
-          (wrapperX, wrapperY, wrapperWidth, wrapperHeight) => {
-            const inViewport = Utils.isInViewport(
-              info.viewportOffset,
-              info.viewportHeight,
-              wrapperY,
-              wrapperHeight,
-              this.props.preTriggerRatio
-            )
-            this.setState({
-              inViewport,
-            })
+          (offsetX, offsetY, width, height) => {
+            this._isMounted &&
+              this.setState({
+                componentOffset: offsetY,
+                componentHeight: height,
+                inViewport: Utils.isInViewport(
+                  info.viewportOffset,
+                  info.viewportHeight,
+                  offsetY,
+                  height,
+                  this.props.preTriggerRatio
+                ),
+              })
           }
         )
       }
@@ -49,10 +71,7 @@ export default WrappedComponent => {
     render() {
       return (
         <WrappedComponent
-          ref={ref => {
-            this.wrapperRef = ref
-            this.nodeHandle = findNodeHandle(ref)
-          }}
+          ref={ref => (this.nodeHandle = findNodeHandle(ref))}
           {...this.props}
           inViewport={this.state.inViewport}
         />
