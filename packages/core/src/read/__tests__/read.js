cf9ee056bf803d32467cdd0acf2b6bdbbb4afbd4
Ognen Ivanovski <ognen.ivanovski@me.com>
2017-10-12 14:49:40 +0200

    Refresh effect, explicit read actions, better HTTP methods (#66)
    * A new `readRefresh` action and a corresponding effect has been added. Triggering this action allows one to *refresh* an already read element. 
       * The effect will expose a `refreshing` property in the element's metadata while the refresh is running (neat for showing a loading indicator)
       * Read Failures are ignored (logged; we might update the metadata if this becomes a requirement)
    
    * Reads have been re-implemented in terms of effects and udpates and *redux-saga* has been dropped as a dependency 🎉 
    * Explicit action types have been added for all the read actions (found in `actions.types.reads`). Explicit action creators are added as well in `actions` for `read` and `readRefresh`.
    * The http module has been touched up
      * `http.execute` method for running arbitrary http calls
      * `http.get` and `http.post` have been added
    * Effects and read log errors better 
    
    BREAKING CHANGE: 
    
    * read action types have changed. Now are namespaced and can be found in `actions.types.read`. If you had any middleware listening on these actions, you might want to check it.
    * To better support API growth, he signature of readers has changed from `(string, boolean) => Promise<ReadResponse>` to `(string, object) => Promise<ReadResponse>`
       * previously a boolean flag indicating whether the read should issue a revalidating http request was passed as a second argument; now this flag can be found inside the second arg like so: 
    ```
     function httpRead(uri, opts) {
       const { revalidate } = opts
      //...
    }
    ```

--- packages/core/src/read/__tests__/read.js
+++ packages/core/src/read/__tests__/read.js
@@ -6,10 +6,15 @@ import * as Subsystem from '../../subsystem'
 import * as Kernel from '../../kernel'
 import enrich from '../../enrich'
 import transform from '../../transform'
+import update from '../../update'
+import effect from '../../effect'
 import read from '..'
 
 import * as action from '../../action'
 import * as data from '../../data'
+
+import * as readActions from '../actions'
+import * as propNames from '../../propNames'
 
 describe('Read Subsytem', () => {
   const app = Subsystem.create(() => ({
@@ -23,15 +28,22 @@ describe('Read Subsytem', () => {
     })
   )
 
+  app.read.register(/failure.json$/, u => Promise.reject(new Error('fail')))
+
   const kernel = Kernel.create(
-    [enrich, transform, read, app],
+    [enrich, transform, effect, update, read, app],
     {
       kind: 'app',
-      '@@girders-elements/children': 'content',
+      [propNames.children]: ['content', 'failure'],
 
       content: {
         kind: ['__read', 'scene'],
         uri: 'https://netcetera.com/test.json',
+      },
+
+      failure: {
+        kind: ['__read', 'scene'],
+        uri: 'https://netcetera.com/failure.json',
       },
     },
     {
@@ -44,15 +56,13 @@ describe('Read Subsytem', () => {
   it('processes reads successfully', async () => {
     let content = kernel.query(['content'])
 
-    const readAction = {
-      type: 'READ',
-      uri: content.get('uri'),
+    const readAction = readActions.read(content.get('uri'), {
       revalidate: content.get('revalidate'),
-    }
+    })
 
     kernel.dispatch(action.atCursor(content, readAction))
 
-    expect(kernel.query(['content', 'kind'])).toEqualI(
+    expect(List(kernel.query(['content', 'kind']))).toEqualI(
       List.of('__loading', 'scene')
     )
 
@@ -62,8 +72,112 @@ describe('Read Subsytem', () => {
     expect(data.isExactlyOfKind('scene', content)).toBeTruthy()
     expect(content.get('title')).toEqual('Scene Title')
   })
+
+  it('handles failures', async () => {
+    let content = kernel.query(['failure'])
+
+    const readAction = readActions.read(content.get('uri'), {
+      revalidate: content.get('revalidate'),
+    })
+
+    kernel.dispatch(action.atCursor(content, readAction))
+
+    await sleep(60)
+
+    content = kernel.query(['failure'])
+    expect(data.isOfKind('__error', content)).toBeTruthy()
+  })
 })
 
+describe('Refreshing', () => {
+  const app = Subsystem.create(() => ({
+    name: 'app',
+  }))
+
+  let counter = 0
+  app.read.register(/test.json$/, u => {
+    counter += 1
+
+    return Promise.resolve({
+      value: { kind: 'scene', title: `Scene Title ${counter}` },
+      meta: app.read.http.responseMeta({ url: u }),
+    })
+  })
+
+  const refresher = jest.fn()
+  refresher.mockReturnValue(
+    Promise.resolve({
+      value: { kind: 'scene', title: 'Scene Title X' },
+      meta: app.read.http.responseMeta({ url: 'https://netcetera.com/x.json' }),
+    })
+  )
+
+  app.read.register(/x.json$/, refresher)
+
+  app.read.register(/fail.json$/, () => Promise.reject(new Error('fail')))
+
+  afterEach(() => {
+    refresher.mockClear()
+  })
+
+  const kernel = Kernel.create([enrich, transform, effect, update, read, app], {
+    kind: 'app',
+    [propNames.children]: ['content'],
+
+    content: {
+      kind: ['__read', 'scene'],
+      uri: 'https://netcetera.com/test.json',
+    },
+  })
+
+  test('refresh', async () => {
+    let content = kernel.query(['content'])
+
+    kernel.dispatch(
+      action.atCursor(content, readActions.read(content.get('uri')))
+    )
+
+    await sleep(60)
+
+    content = kernel.query(['content'])
+    expect(content.get('title')).toEqual('Scene Title 1')
+
+    kernel.dispatch(action.atCursor(content, readActions.readRefresh()))
+
+    await sleep(60)
+
+    content = kernel.query(['content'])
+    expect(content.get('title')).toEqual('Scene Title 2')
+
+    kernel.dispatch(
+      action.atCursor(
+        content,
+        readActions.readRefresh('https://netcetera.com/x.json')
+      )
+    )
+
+    await sleep(60)
+
+    content = kernel.query(['content'])
+    expect(content.get('title')).toEqual('Scene Title X')
+
+    expect(refresher.mock.calls[0][0]).toEqual('https://netcetera.com/x.json')
+    expect(refresher.mock.calls[0][1]).toMatchObject({ revalidate: true })
+
+    kernel.dispatch(
+      action.atCursor(
+        content,
+        readActions.readRefresh('https://netcetera.com/fail.json')
+      )
+    )
+
+    await sleep(60)
+
+    content = kernel.query(['content'])
+    expect(content.get('title')).toEqual('Scene Title X')
+    expect(content.getIn([propNames.metadata, 'refreshing'])).not.toBeTruthy()
+  })
+})
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms))
 }
---
+++ packages/core/src/read/actions.js
@@ -0,0 +1,30 @@
+'use strict'
+
+export const types = {
+  read: '@@girders-elements/actions.read',
+  readRefresh: '@@girders-elements/actions.read.refresh',
+  setRefreshing: '@@girders-elements/actions.read.setRefreshing',
+  setLoading: '@@girders-elements/actions.read.setLoading',
+  apply: '@@girders-elements/actions.read.apply',
+  fail: '@@girders-elements/actions.read.fail',
+}
+
+export function read(uri, opts) {
+  const defaults = {
+    revalidate: false,
+  }
+
+  return {
+    ...defaults,
+    ...opts,
+    uri,
+    type: types.read,
+  }
+}
+
+export function readRefresh(uri = undefined) {
+  return {
+    type: types.readRefresh,
+    uri,
+  }
+}
