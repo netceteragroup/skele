e72e3239afadaaab5052b38d76defbcd347a4d4b
Ognen Ivanovski <ognen.ivanovski@netcetera.com>
2017-11-08 11:46:17 +0100

    chore(config) use === for comparison

--- packages/config/src/utils/freeze.js
+++ packages/config/src/utils/freeze.js
@@ -1,7 +1,7 @@
 export default function deepFreeze(obj) {
   Object.entries(obj).forEach(subObj => {
     const prop = subObj[1]
-    if (typeof prop == 'object' && prop !== null) deepFreeze(prop)
+    if (typeof prop === 'object' && prop !== null) deepFreeze(prop)
   })
   return Object.freeze(obj)
 }
