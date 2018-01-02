a49d9b547295437996e07db6ea32add456f4319f
Andon Sikavica <sikavica@gmail.com>
2018-01-02 11:07:13 +0100

    Implement zip.editCond (#89)
    Utility function that mimics the behavior of transformers: a way to walk the element tree and perform edits for given patterns.
    
    The function has the signature:
    ```javascript
    editCond :: patterns -> loc -> loc
    ```
    Where patterns is an array of `[pattern, editFn]` touples.
    A pattern can be either a function, in which case it is treated as a predicate; alternatively it can be an element kind expression, in which case it will match all elements of that kind.
    
    e.g.
    ```javascript
    zip.editCond(
      [
        [e => e.has('property'), e => e.set('updated', true),
        ['__read',  e => e.set('isRead', true),
        [['document', 'article'], e => e.set('isDocumentArticle', true)
      ],
     initialLocation
    )
    ```
    
    Closes #81

---
+++ packages/core/src/zip/edit.js
@@ -0,0 +1,21 @@
+'use strict'
+
+import { curry } from 'ramda'
+import { isOfKind } from '../data'
+import { postWalk } from '../zip'
+
+export const editCond = curry((patterns, zipper) =>
+  postWalk(el => {
+    patterns.forEach(pattern => {
+      const pred = pattern[0]
+      const updateFn = pattern[1]
+      if (
+        (typeof pred === 'function' && pred(el)) ||
+        (typeof pred !== 'function' && isOfKind(pred, el))
+      ) {
+        el = updateFn(el)
+      }
+    })
+    return el
+  }, zipper)
+)
