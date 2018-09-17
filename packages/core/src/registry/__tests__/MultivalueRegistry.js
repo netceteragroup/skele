5b9380a1cd5cc17542cdf6fff52049a0b42194f6
Ognen Ivanovski <ognen.ivanovski@netcetera.com>
2018-09-17 14:27:44 +0200

    Better tests (multiple values on single entry)

--- packages/core/src/registry/__tests__/MultivalueRegistry.js
+++ packages/core/src/registry/__tests__/MultivalueRegistry.js
@@ -23,12 +23,13 @@ describe('MultivalueRegistry', () => {
   it('prepends matching values of lesser specificity to the result', () => {
     registry.register('a', 1)
     registry.register(['a', 'b'], 2)
+    registry.register(['a', 'b'], 20)
     registry.register(['a', 'b', 'c'], 3)
     registry.register('a', 4)
     registry.register(['a', 'b'], 5)
 
     expect(registry.get('a')).toEqualI(List.of(1, 4))
-    expect(registry.get(['a', 'b'])).toEqualI(List.of(1, 4, 2, 5))
-    expect(registry.get(['a', 'b', 'c'])).toEqualI(List.of(1, 4, 2, 5, 3))
+    expect(registry.get(['a', 'b'])).toEqualI(List.of(1, 4, 2, 20, 5))
+    expect(registry.get(['a', 'b', 'c'])).toEqualI(List.of(1, 4, 2, 20, 5, 3))
   })
 })
