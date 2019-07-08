a54b7eba2d29864a6170b81514543817f414720b
Ognen Ivanovski <ognen.ivanovski@netcetera.com>
2019-07-08 16:20:53 +0200

    Fix invariant

--- packages/system/src/invariant.js
+++ packages/system/src/invariant.js
@@ -1,32 +1,32 @@
 'use strict'
-const NODE_ENV = process.env.NODE_ENV
+let invariant
 
-export default function(condition, format, a, b, c, d, e, f) {
-  if (NODE_ENV !== 'production') {
-    if (format === undefined) {
-      throw new Error('invariant requires an error message argument')
+if (process.env.NODE_ENV !== 'production') {
+  invariant = function(condition, format, a, b, c, d, e, f) {
+    if ((typeof condition === 'function' && !condition()) || !condition) {
+      var error
+      if (format === undefined) {
+        error = new Error(
+          'Minified exception occurred; use the non-minified dev environment ' +
+            'for the full error message and additional helpful warnings.'
+        )
+      } else {
+        var args = [a, b, c, d, e, f]
+        var argIndex = 0
+        error = new Error(
+          format.replace(/%s/g, function() {
+            return args[argIndex++]
+          })
+        )
+        error.name = 'Invariant Violation'
+      }
+
+      error.framesToPop = 1 // we don't care about invariant's own frame
+      throw error
     }
   }
-
-  if ((typeof condition === 'function' && !condition()) || !condition) {
-    var error
-    if (format === undefined) {
-      error = new Error(
-        'Minified exception occurred; use the non-minified dev environment ' +
-          'for the full error message and additional helpful warnings.'
-      )
-    } else {
-      var args = [a, b, c, d, e, f]
-      var argIndex = 0
-      error = new Error(
-        format.replace(/%s/g, function() {
-          return args[argIndex++]
-        })
-      )
-      error.name = 'Invariant Violation'
-    }
-
-    error.framesToPop = 1 // we don't care about invariant's own frame
-    throw error
-  }
+} else {
+  invariant = function() {}
 }
+
+export default invariant
